54055308c40ba2786328dcb42eecd3ac0e21d1ac
AndreaGul <andreagulino99@gmail.com>
2024-03-06 13:03:16 +0100

    feat:header sotto pagine

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,18 +1,21 @@
 <script>
 import AppJumbotron from './AppJumbotron.vue';
 import AppCardProfessional from './AppCardProfessional.vue';
+import AppHeaderSubPages from './AppHeaderSubPages.vue';
 export default {
   name: 'Main',
 
   components: {
     AppJumbotron,
     AppCardProfessional,
+    AppHeaderSubPages,
   },
 };
 </script>
 
 <template>
   <AppJumbotron></AppJumbotron>
+  <AppHeaderSubPages></AppHeaderSubPages>
   <div>
     main
     <AppCardProfessional></AppCardProfessional>
