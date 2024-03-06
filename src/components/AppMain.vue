e51beae63c30dc996d5a9a5b7ed46f02b385175c
FilippoBersan <filippobersan99@gmail.com>
2024-03-06 11:07:04 +0100

    feat: card professionista

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,17 +1,22 @@
 <script>
 import AppJumbotron from './AppJumbotron.vue';
+import AppCardProfessional from './AppCardProfessional.vue';
 export default {
   name: 'Main',
 
   components: {
     AppJumbotron,
+    AppCardProfessional,
   },
 };
 </script>
 
 <template>
   <AppJumbotron></AppJumbotron>
-  <div>main</div>
+  <div>
+    main
+    <AppCardProfessional></AppCardProfessional>
+  </div>
 </template>
 
 <style scoped></style>
