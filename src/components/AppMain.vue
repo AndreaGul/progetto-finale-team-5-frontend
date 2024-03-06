7b4ad315515f82057a94fc921e66fe41a0fb7c84
matteocicigoi <matteo.cicigoi1@hotmail.it>
2024-03-06 12:31:00 +0100

    feat: ricerca

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,21 +1,41 @@
 <script>
 import AppJumbotron from './AppJumbotron.vue';
 import AppCardProfessional from './AppCardProfessional.vue';
+import axios from "axios";
 export default {
   name: 'Main',
 
+  data() {
+    return {
+      professionals : []
+    }
+  },
   components: {
     AppJumbotron,
     AppCardProfessional,
   },
+
+  methods:{
+    search(id){
+      if(id !== ''){
+        axios
+        .get("http://127.0.0.1:8000/api/professionals/" + id)
+        .then((response) => {
+          this.professionals = response.data.data.data;
+        });
+      }
+    }
+  }
 };
 </script>
 
 <template>
-  <AppJumbotron></AppJumbotron>
-  <div>
+  <AppJumbotron @search="search"></AppJumbotron>
+  <div class="container pt-5">
+    <div class="row d-flex flex-wrap">
+      <AppCardProfessional v-for="professional in professionals" :slug="professional.slug" :specializations="professional.specializations" :photo="professional.photo"></AppCardProfessional>
+    </div>
     main
-    <AppCardProfessional></AppCardProfessional>
   </div>
 </template>
 
