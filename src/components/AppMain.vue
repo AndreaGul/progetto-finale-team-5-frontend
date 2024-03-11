25df45a6f5def2b2829925a9edad526ac24341b4
AndreaGul <andreagulino99@gmail.com>
2024-03-11 15:31:08 +0100

    fix:ricerca pagina home

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,17 +1,17 @@
 <script>
-import AppJumbotron from "./AppJumbotron.vue";
-import AppCardProfessional from "./AppCardProfessional.vue";
-import store from "../../store";
+import AppJumbotron from './AppJumbotron.vue';
+import AppCardProfessional from './AppCardProfessional.vue';
+import store from '../../store';
 // da mettere nella pagina info
 
 // import AppInfoSingleProfessional from './AppInfoSingleProfessional.vue';
 // import AppReviews from './AppReviews.vue';
 // import AppDetailInfoProfessional from './AppDetailInfoProfessional.vue';
 
-import axios from "axios";
+import axios from 'axios';
 
 export default {
-  name: "Main",
+  name: 'Main',
 
   data() {
     return {
@@ -31,10 +31,13 @@ export default {
   methods: {
     search() {
       this.$router.push({
-        name: "professionalList",
+        name: 'professionalList',
         params: { id: this.store.specializationsName },
       });
     },
+  },
+  created() {
+    this.store.specializationsName = '';
   },
 };
 </script>
