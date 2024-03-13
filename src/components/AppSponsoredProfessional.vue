61673dc749eab74379ad7d73f6d3a180982d4786
AndreaGul <andreagulino99@gmail.com>
2024-03-13 17:58:09 +0100

    feat:posizionamento degli elementi nella home

--- src/components/AppSponsoredProfessional.vue
+++ src/components/AppSponsoredProfessional.vue
@@ -21,80 +21,92 @@ export default {
 </script>
 
 <template>
-  <!-- In evidenza -->
-  <div class="container p-3">
-    <h4 class="text-center text-uppercase titolo">
-      Cerca il miglior professionista che fa per te
-    </h4>
+  <div class="container_sponsorizzati">
+    <!-- In evidenza -->
+    <div class="container p-3">
+      <h4 class="text-center text-uppercase titolo">
+        Cerca il miglior professionista che fa per te
+      </h4>
 
-    <div class="ag-format-container">
-      <div class="ag-courses_box row g-3">
-        <!-- card singola -->
-        <div
-          class="ag-courses_item col-12 col-md-6 col-xl-4"
-          v-for="sponsor in sponsorProp"
-        >
-          <router-link
-            :to="{
-              name: 'ProfessionalDetail',
-              params: { slug: sponsor.slug },
-            }"
-            @click="getInfo(sponsor.id)"
-            class="text-decoration-none"
+      <div class="ag-format-container">
+        <div class="ag-courses_box row g-3">
+          <!-- card singola -->
+          <div
+            class="ag-courses_item col-12 col-md-6 col-xl-4"
+            v-for="sponsor in sponsorProp"
           >
-            <a href="#" class="ag-courses-item_link text-decoration-none">
-              <div class="ag-courses-item_bg"></div>
+            <router-link
+              :to="{
+                name: 'ProfessionalDetail',
+                params: { slug: sponsor.slug },
+              }"
+              @click="getInfo(sponsor.id)"
+              class="text-decoration-none"
+            >
+              <a href="#" class="ag-courses-item_link text-decoration-none">
+                <div class="ag-courses-item_bg"></div>
 
-              <div
-                class="ag-courses-item_title row mb-3 justify-content-md-center"
-              >
-                <div class="user-card-img col-5 col-md-6 col-lg-4">
-                  <img
-                    v-if="sponsor.photo && sponsor.photo.startsWith('uploads')"
-                    :src="'http://127.0.0.1:8000/storage/' + sponsor.photo"
-                    alt=""
-                  />
-                  <img v-else-if="sponsor.photo" :src="sponsor.photo" alt="" />
-                  <img
-                    v-else
-                    src="https://static.vecteezy.com/system/resources/thumbnails/019/879/186/small/user-icon-on-transparent-background-free-png.png"
-                    alt=""
-                  />
+                <div
+                  class="ag-courses-item_title row mb-3 justify-content-md-center"
+                >
+                  <div class="user-card-img col-5 col-md-6 col-lg-4">
+                    <img
+                      v-if="
+                        sponsor.photo && sponsor.photo.startsWith('uploads')
+                      "
+                      :src="'http://127.0.0.1:8000/storage/' + sponsor.photo"
+                      alt=""
+                    />
+                    <img
+                      v-else-if="sponsor.photo"
+                      :src="sponsor.photo"
+                      alt=""
+                    />
+                    <img
+                      v-else
+                      src="https://static.vecteezy.com/system/resources/thumbnails/019/879/186/small/user-icon-on-transparent-background-free-png.png"
+                      alt=""
+                    />
+                  </div>
+
+                  <div class="col-7 col-md-6 col-lg-8">
+                    {{ sponsor.user.name }} {{ sponsor.user.surname }}
+                  </div>
                 </div>
 
-                <div class="col-7 col-md-6 col-lg-8">
-                  {{ sponsor.user.name }} {{ sponsor.user.surname }}
-                </div>
-              </div>
-
-              <div class="ag-courses-item_date-box">
-                <div class="card-description">Specializzazioni:</div>
-                <p class="specialization-list">
-                  <span
-                    v-for="(specialization, index) in sponsor.specializations"
-                    :key="index"
-                  >
-                    {{ specialization.name }}
-                    <span v-if="index < sponsor.specializations.length - 1"
-                      >,
+                <div class="ag-courses-item_date-box">
+                  <div class="card-description">Specializzazioni:</div>
+                  <p class="specialization-list">
+                    <span
+                      v-for="(specialization, index) in sponsor.specializations"
+                      :key="index"
+                    >
+                      {{ specialization.name }}
+                      <span v-if="index < sponsor.specializations.length - 1"
+                        >,
+                      </span>
                     </span>
-                  </span>
-                </p>
-                <!-- Dettaglio Professionista -->
+                  </p>
+                  <!-- Dettaglio Professionista -->
 
-                <!-- Fine Dettaglio Professionista -->
-              </div>
-            </a>
-          </router-link>
+                  <!-- Fine Dettaglio Professionista -->
+                </div>
+              </a>
+            </router-link>
+          </div>
+          <!-- card singola -->
         </div>
-        <!-- card singola -->
       </div>
     </div>
+    <!-- Fine In evidenza -->
   </div>
-  <!-- Fine In evidenza -->
 </template>
 
 <style scoped>
+.container_sponsorizzati {
+  background-color: #bfdbf7;
+}
+
 .titolo {
   font-family: 'Share Tech Mono', monospace;
   font-weight: 600;
