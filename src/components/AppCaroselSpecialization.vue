c16b0b76459a503a4a85ad1a36417768b5513781
AndreaGul <andreagulino99@gmail.com>
2024-03-14 10:00:47 +0100

    feat:componente carosello specialization inserito in pagina

--- src/components/AppCaroselSpecialization.vue
+++ src/components/AppCaroselSpecialization.vue
@@ -1,28 +1,41 @@
 <script>
-import store from "../../store";
+import store from '../../store';
 export default {
-  name: "SectionThreeSpecializations",
+  name: 'prova',
   data() {
     return {
+      active: 2,
       store,
       specializations: [
         {
-          name: "web development",
+          name: 'web development',
           description:
-            "Il processo di creazione e manutenzione di siti e applicazioni web.",
-          specialization_name: "Web Development",
+            'Il processo di creazione e manutenzione di siti e applicazioni web.',
+          specialization_name: 'Web Development',
         },
         {
-          name: "data analysis",
+          name: 'data analysis',
           description:
-            "Il processo di esplorazione, interpretazione dei dati e analisi.",
-          specialization_name: "Data Analysis",
+            'Il processo di esplorazione, interpretazione dei dati e analisi.',
+          specialization_name: 'Data Analysis',
         },
         {
-          name: "machine learning",
+          name: 'machine learning',
           description:
             "L'arte di far imparare ai computer senza istruzioni esplicite.",
-          specialization_name: "Machine Learning",
+          specialization_name: 'Machine Learning',
+        },
+        {
+          name: 'artificial intelligence',
+          description:
+            'Sviluppo di sistemi in grado di eseguire compiti richiedenti intelligenza umana.',
+          specialization_name: 'Artificial Intelligence',
+        },
+        {
+          name: 'mobile development',
+          description:
+            'Processo di creazione di applicazioni software per dispositivi mobili.',
+          specialization_name: 'Mobile Development',
         },
       ],
     };
@@ -31,9 +44,12 @@ export default {
     search(name) {
       this.store.specializationsName = name;
       this.$router.push({
-        name: "professionalList",
+        name: 'professionalList',
         params: { id: this.store.specializationsName },
       });
+    },
+    changeActive(index) {
+      this.active = index;
     },
   },
 };
@@ -41,30 +57,104 @@ export default {
 
 <template>
   <div class="contain">
-    <div
-      v-for="(specialization, index) in specializations"
-      class="container-r col-3"
-    >
-      <div class="wrapper-r">
-        <div :class="'banner-image-' + index"></div>
-        <h1>{{ specialization.name }}</h1>
-        <p>
-          {{ specialization.description }}
-        </p>
-      </div>
-      <div class="button-wrapper">
-        <button
-          class="btn outline"
-          @click="search(specialization.specialization_name)"
+    <div class="carousel-container">
+      <main id="carousel" :class="'classecarousel' + (active + 1)">
+        <div
+          v-for="(specialization, index) in specializations"
+          class="container-r col-3 item"
+          @click="changeActive(index)"
         >
-          Cerca il professionista che fa per te
-        </button>
-      </div>
+          <div class="wrapper-r">
+            <div :class="'banner-image banner-' + index"></div>
+            <h1>{{ specialization.name }}</h1>
+            <p>
+              {{ specialization.description }}
+            </p>
+          </div>
+          <div class="button-wrapper">
+            <button
+              class="btn outline"
+              @click="search(specialization.specialization_name)"
+            >
+              Cerca il professionista che fa per te
+            </button>
+          </div>
+        </div>
+      </main>
     </div>
   </div>
 </template>
 
 <style scoped lang="scss">
+.carousel-container {
+  height: 700px;
+  margin: 0;
+  align-items: center;
+  justify-items: center;
+}
+
+main#carousel {
+  grid-row: 1 / 2;
+  grid-column: 1 / 8;
+  width: 100vw;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
+  transform-style: preserve-3d;
+  perspective: 600px;
+  --items: 5;
+  --middle: 3;
+}
+
+div.item {
+  position: absolute;
+  width: 300px;
+  height: 600px;
+  --r: calc(var(--position) - var(--offset));
+  --abs: max(calc(var(--r) * -1), var(--r));
+  transition: all 0.25s linear;
+  transform: rotateY(calc(-10deg * var(--r)))
+    translateX(calc(-300px * var(--r)));
+  z-index: calc((var(--position) - var(--abs)));
+}
+
+div.item:nth-of-type(1) {
+  --offset: 1;
+}
+div.item:nth-of-type(2) {
+  --offset: 2;
+}
+div.item:nth-of-type(3) {
+  --offset: 3;
+}
+div.item:nth-of-type(4) {
+  --offset: 4;
+}
+div.item:nth-of-type(5) {
+  --offset: 5;
+}
+
+.classecarousel1 {
+  --position: 1;
+}
+
+.classecarousel2 {
+  --position: 2;
+}
+
+.classecarousel3 {
+  --position: 3;
+}
+
+.classecarousel4 {
+  --position: 4;
+}
+.classecarousel5 {
+  --position: 5;
+}
+
 .contain {
   /* solid background */
   background: rgb(0, 212, 255);
@@ -109,39 +199,38 @@ export default {
   height: 100%;
 }
 
-.banner-image-0 {
+.banner-image {
+  background-position: center;
+  background-size: cover;
+  height: 250px;
+  width: 100%;
+  border-radius: 12px;
+  border: 1px solid rgba(255, 255, 255, 0.255);
+}
+
+.banner-0 {
   background-image: url(https://miro.medium.com/v2/resize:fit:12000/0*tQQ7SLPOJfxaG4ZY);
-  background-position: center;
-  background-size: cover;
-  height: 250px;
-  width: 100%;
-  border-radius: 12px;
-  border: 1px solid rgba(255, 255, 255, 0.255);
 }
 
-.banner-image-1 {
+.banner-1 {
   background-image: url(https://www.simplilearn.com/ice9/free_resources_article_thumb/data_analyticstrendsmin.jpg);
-  background-position: center;
-  background-size: cover;
-  height: 250px;
-  width: 100%;
-  border-radius: 12px;
-  border: 1px solid rgba(255, 255, 255, 0.255);
 }
 
-.banner-image-2 {
+.banner-2 {
   background-image: url(https://www.it-impresa.it/wp-content/uploads/Machine-Learning-for-Data-Analytics-Concept-800x469.jpg.webp);
-  background-position: center;
-  background-size: cover;
-  height: 250px;
-  width: 100%;
-  border-radius: 12px;
-  border: 1px solid rgba(255, 255, 255, 0.255);
+}
+
+.banner-3 {
+  background-image: url(https://img.freepik.com/free-vector/ai-technology-microchip-background-vector-digital-transformation-concept_53876-117808.jpg);
+}
+
+.banner-4 {
+  background-image: url(https://evatronix.com/images/en/offer/product-design/software-development/Evatronix_Mobile_applications_01_1920x1080.jpg);
 }
 
 h1 {
   margin-top: 20px;
-  font-family: "Righteous", sans-serif;
+  font-family: 'Righteous', sans-serif;
   color: rgba(255, 255, 255, 0.98);
   text-transform: uppercase;
   font-size: 30px;
@@ -149,7 +238,7 @@ h1 {
 
 p {
   color: #fff;
-  font-family: "Lato", sans-serif;
+  font-family: 'Lato', sans-serif;
   text-align: center;
   font-size: 0.8rem;
   line-height: 150%;
