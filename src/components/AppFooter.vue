6732987c36930fdc465410ce55e5cae1d3889f52
FilippoBersan <filippobersan99@gmail.com>
2024-03-15 12:39:42 +0100

    feat: footer responsive

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,11 +1,11 @@
 <script>
 export default {
-  name: "Footer",
+  name: 'Footer',
   methods: {
-    toTop(){
+    toTop() {
       document.documentElement.scrollTop = 0;
-    }
-  }
+    },
+  },
 };
 </script>
 
@@ -14,8 +14,10 @@ export default {
     <div class="footer-up">
       <div class="container d-flex justify-content-between">
         <div class="footer-links">
-          <ul>
-            <li><router-link to="/" class="btn btn-home" @click="toTop()">Home</router-link></li>
+          <ul class="info-c">
+            <li>
+              <router-link to="/" class="" @click="toTop()">Home</router-link>
+            </li>
             <li><a href="#">Chi siamo</a></li>
             <li><a href="#">Servizi</a></li>
             <li><a href="#">Contatti</a></li>
@@ -25,7 +27,7 @@ export default {
           <img src="/BOOLTECHSLOGO.png" alt="" />
         </div>
         <div class="footer-social">
-          <ul>
+          <ul class="social-c">
             <li>
               <a href="#"><i class="fab fa-facebook"></i></a>
             </li>
@@ -50,11 +52,12 @@ export default {
 
 <style scoped lang="scss">
 .logo img {
-  max-height: 45px;
-  margin: 0;
-  padding: 0;
+  max-height: 30px;
+  position: relative;
+  right: 40px;
 }
 .footer-up {
+  margin-top: 20px;
   background-color: #022b3a;
   color: #fff;
   padding: 30px;
@@ -98,5 +101,58 @@ export default {
   background-color: #123c46;
   color: white;
   padding: 15px 0;
+}
+
+.social-c {
+  display: flex;
+  gap: 30px;
+}
+
+@media (max-width: 992px) {
+  .social-c {
+    flex-direction: column;
+    gap: 5px;
+  }
+
+  .info-c {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+  }
+}
+
+@media (max-width: 992px) {
+  .container {
+    flex-direction: column;
+    justify-content: center;
+  }
+
+  .social-c {
+    flex-direction: row;
+    gap: 5px;
+    justify-content: center;
+    position: relative;
+    right: 10px;
+  }
+
+  .info-c {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    gap: 10px;
+    position: relative;
+    left: 8px;
+  }
+
+  .logo {
+    display: flex;
+    justify-content: center;
+    margin-bottom: 20px;
+    position: relative;
+    left: 35px;
+  }
+  a {
+    font-size: 14px;
+  }
 }
 </style>
